9b5f94f9391fd4753ee9b376b8fb3a48f2f2dd8a
CheongRyoung <93980966+CheongRyoung@users.noreply.github.com>
2023-12-14 23:53:43 +0900

    [Bronze I] Title: 단어 공부, Time: 264 ms, Memory: 35996 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -2,17 +2,8 @@ const fs = require('fs');
 const S = fs.readFileSync("/dev/stdin").toString().trim(); ///dev/stdin
 
 const words = S.toLowerCase();
-let map = words.split("")
-    .reduce((p, c,i) => {
-        let value = p.get(c);
-        value = value ? value + 1 : 1;
-        p.set(c, value);
-        return p;
-}, new Map());
-const mapToArray = [...map];
-mapToArray.sort((a, b) => b[1] - a[1]);
-if(mapToArray.length === 1 || mapToArray[0][1] !== mapToArray[1][1]) {
-    console.log(mapToArray[0][0].toUpperCase()); 
-} else {
-    console.log("?"); 
-}
+const object = {}
+words.split("").forEach((v) => {object[v] = object[v] ? object[v]+1 : 1})
+const maxCnt = Math.max(...Object.values(object));
+const filterWords = Object.keys(object).filter((v) => object[v] === maxCnt);
+console.log(filterWords.length > 1 ? "?" : filterWords[0].toUpperCase());
